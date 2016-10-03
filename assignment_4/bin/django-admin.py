29b29c9841929f998564c2bf564ee07c815d469a
Sheel Doshi <sheelvdoshi@gmail.com>
2016-10-03 06:33:40 +0000

    assignment_4 commit

---
+++ assignment_4/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bryan/repo/CINS465-Fall2016-Lecture-Examples/lecture9/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
